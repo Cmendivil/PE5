37d371b9028793c8fc95b3a490eaa70cf1809d24
Cristian Mendivil <cmendivil7@icloud.com>
2018-09-23 20:54:32 -0600

    rest of the functions added

--- Rectangle.cpp
+++ Rectangle.cpp
@@ -1,6 +1,6 @@
 /*
 Jacob Tran
-Christian
+Cristian Mendivil
 CSCI 3010
 PE5
 */
@@ -10,23 +10,37 @@ PE5
 
 using namespace std;
 
-Rectangle:Rectangle(Point p1, Point p2) {
-	Point p1_ = p1;
-	Point p2_ = p2;
+/*
+number is a positive number that user wants to calculate the factorial of it(numer!) 
+returns factorial of a number
+*/
+unsigned int Factorial( unsigned int number ){
+	if(number == 0){ //base case
+		return 1;
+	}
+	else{
+		return number * Factorial(number-1);
+	}
+	
+}
+
+Rectangle::Rectangle(Point p1, Point p2) {
+	p1_ = p1;
+	p2_ = p2;
 }
 
 /*
 returns the width between p1.x and p2.x
 */
 int Rectangle::GetWidth() {
-	return abs(p1.x - p2.x);
+	return abs(p1_.x - p2_.x);
 }
 
 /*
 returns the height between p1.y and p2.y
 */
 int Rectangle::GetHeight() {
-	return abs(p1.y - p2.y);
+	return abs(p1_.y - p2_.y);
 } 
 
 /*
@@ -34,23 +48,40 @@ other is the other point you're comapring it to
 returns a bool if any point overlaps
 */
 bool Rectangle::Overlaps(Rectangle& other) {
-	if (p1.x > other.p2_.x || other.p1_.x > p2_.x) {
+	if (p1_.x > other.p2_.x || other.p1_.x > p2_.x) {
 		return true;
 	} 
-	if (p1.y < other.p2_.y || other.p1_.y < p2_.y) {
+	if (p1_.y < other.p2_.y || other.p1_.y < p2_.y) {
 		return true;
 	}
 	return false;
 }
 
+/*
+returns the area of the rectangle as an int
+*/
 int Rectangle::CalculateArea() {
-
+	return GetWidth()*GetHeight();
 }
 
+/*
+increase the rectangle size
+*/
 void Rectangle::Expand() {
-
+	p1_.x -= 1;
+	p1_.y -= 1;
+	p2_.x += 1;
+	p2_.y += 1;
+	
 }
 
+/*
+decrease the rectangle size
+*/
 void Rectangle::Shrink() {
+	p1_.x += 1;
+	p1_.y += 1;
+	p2_.x -= 1;
+	p2_.y -= 1;
 	
 }
