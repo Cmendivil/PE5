e779d503d9ca52d8351a9ddd42ecc64b86e2d547
Jacob Tran <31492444+jatr6994@users.noreply.github.com>
2018-09-23 20:11:12 -0600

    added comments and fixed Overlaps

--- Rectangle.cpp
+++ Rectangle.cpp
@@ -15,18 +15,29 @@ Rectangle:Rectangle(Point p1, Point p2) {
 	Point p2_ = p2;
 }
 
+/*
+returns the width between p1.x and p2.x
+*/
 int Rectangle::GetWidth() {
 	return abs(p1.x - p2.x);
 }
 
+/*
+returns the height between p1.y and p2.y
+*/
 int Rectangle::GetHeight() {
 	return abs(p1.y - p2.y);
 } 
 
+/*
+other is the other point you're comapring it to
+returns a bool if any point overlaps
+*/
 bool Rectangle::Overlaps(Rectangle& other) {
-	if (p1.x == other.x && p1.y == other.y) {
+	if (p1.x == other.x || p1.y == other.y) {
 		return true;
-	} else if (p2.x == other.x && p2.y == other.y) {
+	} 
+	if (p2.x == other.x || p2.y == other.y) {
 		return true;
 	}
 	return false;
